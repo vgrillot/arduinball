802ff688187e3bea9f5ee10cf300850a72c17834
vgrillot <GR1Vgithub>
2017-04-21 20:02:32 +0200

    protocol simplification

--- pinball_sw_matrix/protocol.cpp
+++ pinball_sw_matrix/protocol.cpp
@@ -9,6 +9,7 @@
 
 
 #include "protocol.h"
+#include "hw_rules.h"
 
 Protocol::Protocol(Comm *comm, HwRules *rules) {
     this->__input.reserve(255);
@@ -33,32 +34,31 @@ boolean Protocol::run() {
     
     if (this->__comm->readLn()) {
         this->__input = this->__comm->input;
-        if (this->getNextWord() && (this->__nextWord == "RUL")) {
-            if (this->getNextWord() && (this->__nextWord == "CLR")) {
-                // RUL CLR SW COIL
-                if (this->getNextWord())
-                    enableSwitchId = this->__nextWord.toInt();
+        this->__comm->debug("PROTOCOL:" + this->__input);
+        if (this->getNextWord()) {
+            if (this->__nextWord == "RC") {
+                // RUL CLR COIL SW
                 if (this->getNextWord())
                     coilPin = this->__nextWord.toInt();
+                if (this->getNextWord())
+                    enableSwitchId = this->__nextWord.toInt();
                 this->__rules->clearRule(coilPin, enableSwitchId);
-            }
-            else if (this->__nextWord == "ADD") {
+            }            
+            else if (this->__nextWord == "RA") {
                 // RUL ADD TYPE SW COIL SW DURATION
                 if (this->getNextWord())
                     type = (HwRuleType)(this->__nextWord.toInt());
                 if (this->getNextWord())
-                    enableSwitchId = this->__nextWord.toInt();
-                if (this->getNextWord())
                     coilPin = this->__nextWord.toInt();
+                if (this->getNextWord())
+                    enableSwitchId = this->__nextWord.toInt();
                 if (this->getNextWord())
                     disableSwitchId = this->__nextWord.toInt();
                 if (this->getNextWord())
                     duration = this->__nextWord.toInt();
                 this->__rules->addHwRule(type, enableSwitchId, coilPin, disableSwitchId, duration);
-            }
-        }
-        else if (this->__nextWord == "DRV") {
-            if (this->getNextWord() && (this->__nextWord == "PUL")) {
+            }        
+            else if (this->__nextWord == "DP") {
                 // DRV PUL COIL DURATION
                 if (this->getNextWord())
                     coilPin = this->__nextWord.toInt();
@@ -66,24 +66,48 @@ boolean Protocol::run() {
                     duration = this->__nextWord.toInt();
                 this->__rules->addPulse(coilPin, duration);
             }
-            else if (this->__nextWord == "ENB") {
+            else if (this->__nextWord == "DE") {
                 // DRV ENB
                 if (this->getNextWord())
                     coilPin = this->__nextWord.toInt();
                 this->__rules->addEnable(coilPin);
             }
-            else if (this->__nextWord == "DIS") {
+            else if (this->__nextWord == "DD") {
                 // DRV DIS
                 if (this->getNextWord())
                     coilPin = this->__nextWord.toInt();
                 this->__rules->addDisable(coilPin);
             }
+            else
+                this->__comm->debug("Unknown command " + this->__nextWord);
         }
-    }
+        this->__input = "";  //clean unread buffer...
+        this->__nextWord = "";
+     }
 }
 
 
+/*
+ * getNextWord()
+ * return true if a word is found in __input buffer
+ * set __nextWord value
+ * !!170419:VG:Creation
+ */
 boolean Protocol::getNextWord() {
-
+  if (this->__input.length() > 0) {
+    int i = this->__input.indexOf(":");
+    if (i == -1) {
+      this->__nextWord = this->__input;
+      this->__input = ""; 
+    }
+    else
+    {
+      this->__nextWord = this->__input.substring(0, i);
+      this->__input = this->__input.substring(i+1);
+    }
+    return true;
+  }
+  else
+    return false;
 }
 
