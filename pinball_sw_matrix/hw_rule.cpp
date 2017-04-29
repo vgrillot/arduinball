dea3678c604991e0e48eb0a44260a92cd6647034
vgrillot <GR1Vgithub>
2017-04-29 16:18:57 +0200

    add rule_type_need_sw()

--- pinball_sw_matrix/hw_rule.cpp
+++ pinball_sw_matrix/hw_rule.cpp
@@ -8,5 +8,24 @@
 #include "hw_rule.h"
 
 
+/*
+ * rule_type_need_sw()
+ * return true if the rule need a switch to run
+ * to avoid bad setup
+ */
+bool rule_type_need_sw(HwRuleType rt) {
+  switch(rt) {
+    case hw_rule_pulse_on_hit_rule:
+    case hw_rule_pulse_on_hit_and_release_rule:
+    case hw_rule_pulse_on_hit_and_enable_and_release_rule:
+    case hw_rule_pulse_on_hit_and_enable_and_release_and_disable_rule:
+      return true;
+    case hw_rule_enable:
+    case hw_rule_pulse:
+      return false;
+  }
+}
+
+
 
 
